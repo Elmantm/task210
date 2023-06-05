4f1fe195ca4f1cab1aba6caf83b1fc0e0ee8e923
ElmanManmedov <elmantm@code.edu.az>
2023-06-05 10:22:18 +0400

    new

---
+++ script.js
@@ -0,0 +1,21 @@
+function login{
+    let email = 'elmantm@gmail.com';
+    let password = '12345678'
+}
+
+let result1=prompt('email')
+let result2=prompt('password')
+let button=prompt('login')
+
+if(result1=='elmantm@gmail.com'){
+    alert(true);
+}else{
+    alert(false);
+}
+
+if(result2=='12345678'){
+    alert(true);
+}else{
+    alert(false);
+}
+
